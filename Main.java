e41dd735a703498acffedf83a9ccaa81c669b664
Azmir669 <azmirvali5@gmail.com>
2025-06-10 19:54:00 +0530

    shadowing

--- Main.java
+++ Main.java
@@ -1,6 +1,19 @@
 
-class Main{
+class Shadowing{
+    String name="azmir";
+    class innerShadowing{
+        String name="vali";
+        public void display(){
+        System.out.print(name);
+        System.out.print(this.name);
+        System.out.print(Shadowing.this.name);}
+    }
+}
+public class Main{
     public static void main(String[] args){
-
+        Shadowing s=new Shadowing();
+ Shadowing.innerShadowing inner=s. new innerShadowing();
+ inner.display();
+ 
     }
 }
